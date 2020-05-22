7d9df5349447f293876d42b76aa82a6ef919e743
danuluncasu <danuluncasu@icloud.com>
2020-05-22 11:43:32 +0300

    lab 3 and 4 added

---
+++ lab3-4/Assets/ConeBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeBehavior : MonoBehaviour {    
+    void Start() {
+        this.GetComponent<Renderer> ().material.color = Color.blue;
+        InvokeRepeating("CustomUpdate", 1.0f, 1.0f);
+    }
+
+    void CustomUpdate()
+    {
+        this.transform.position = new Vector3(
+            this.transform.position.x + UnityEngine.Random.Range(-1, 2), 
+            this.transform.position.y + UnityEngine.Random.Range(-1, 2),
+            this.transform.position.z + UnityEngine.Random.Range(-1, 2)
+        );
+    }
+}
---
+++ lab3-4/Assets/SphereBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBehavior : MonoBehaviour
+{
+    void Start() {
+        this.GetComponent<Renderer> ().material.color = Color.green;
+        InvokeRepeating("CustomUpdate", 1.0f, 1.0f);
+    }
+
+    void CustomUpdate()
+    {
+        this.transform.position = new Vector3(
+            this.transform.position.x + UnityEngine.Random.Range(-1, 2), 
+            this.transform.position.y + UnityEngine.Random.Range(-1, 2),
+            this.transform.position.z + UnityEngine.Random.Range(-1, 2)
+        );
+    }
+}
